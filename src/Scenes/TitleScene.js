aca687b54b822245de509ef748785e3ddb79cecc
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2021-01-21 14:41:23 -0300

    Add variables for width and height and use arrow functions

--- src/Scenes/TitleScene.js
+++ src/Scenes/TitleScene.js
@@ -6,25 +6,38 @@ export default class TitleScene extends Phaser.Scene {
   }
 
   preload () {
+    this.width = this.cameras.main.width;
+    this.height = this.cameras.main.height;
   }
 
   create () {
     // Game
+    var assetText = this.make.text({
+      x: this.width / 2,
+      y: this.height / 2 + 50,
+      text: 'Kill zombies of productivity',
+      style: {
+        font: '18px monospace',
+        fill: '#ffffff'
+      }
+    });
+    assetText.setOrigin(0.5, 0.5);
+
     this.gameButton = this.add.sprite(100, 200, 'blueButton1').setInteractive();
     this.centerButton(this.gameButton, 1);
     
     this.gameText = this.add.text(0, 0, 'Play', { fontSize: '32px', fill: '#fff' });
     this.centerButtonText(this.gameText, this.gameButton);
     
-    this.gameButton.on('pointerdown', function (pointer) {
+    this.gameButton.on('pointerdown', (pointer) => {
       this.scene.start('Game');
-    }.bind(this));
+    })
     
-    this.input.on('pointerover', function (event, gameObjects) {
+    this.input.on('pointerover', (event, gameObjects) => {
       gameObjects[0].setTexture('blueButton2');
     });
     
-    this.input.on('pointerout', function (event, gameObjects) {
+    this.input.on('pointerout', (event, gameObjects) => {
       gameObjects[0].setTexture('blueButton1');
     });
   }
@@ -32,7 +45,7 @@ export default class TitleScene extends Phaser.Scene {
   centerButton (gameObject, offset = 0) {
     Phaser.Display.Align.In.Center(
       gameObject,
-      this.add.zone(800/2, 600/2 - offset * 100, 800, 600)
+      this.add.zone(this.width/2, this.height/2 - offset * 100, this.width, this.height)
     );
   }
    
