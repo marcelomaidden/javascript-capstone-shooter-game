0cc9bd90d2e5d8d23f92c8e8a2ac54aa5c2f2718
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2021-01-22 17:16:38 -0300

    Add zombies

--- src/Characters/Zombie.js
+++ src/Characters/Zombie.js
@@ -2,43 +2,65 @@
 import 'phaser';
 
 export default class Zombie extends Phaser.Physics.Arcade.Sprite  {
-  
+  addAnimation() {
+    if (!this.scene.anims.get('zombie_walk')){
+      this.scene.anims.create({
+        key: 'zombie_walk', 
+        frames: [
+          {key: 'zombie1'},
+          {key: 'zombie2'},
+          {key: 'zombie3'},
+          {key: 'zombie4'},
+          {key: 'zombie5'},
+          {key: 'zombie6'},
+          {key: 'zombie7'},
+          {key: 'zombie8'},
+          {key: 'zombie9'},
+          {key: 'zombie10'},
+          {key: 'zombie11'},
+          {key: 'zombie12'},
+          {key: 'zombie13'},
+          {key: 'zombie14'},
+          {key: 'zombie15'},
+          {key: 'zombie16'},
+          {key: 'zombie17'},
+          {key: 'zombie18'},
+          {key: 'zombie19'},
+          {key: 'zombie20'},
+          {key: 'zombie21'},
+          {key: 'zombie22'},
+          {key: 'zombie23'},
+          {key: 'zombie24'},
+        ],
+        frameRate: 8,
+        repeat: -1
+      })
+    }
+  }
+
   constructor(scene, x, y, texture) {
     super(scene, x, y, texture);
+
     scene.physics.world.enableBody(this);
     scene.add.existing(this);
     this.setScale(0.5);  
     this.body.bounce.x = 1;
     this.body.bounce.y = 1;
     this.body.setCollideWorldBounds(true);
-    
-    this.body.setSize(150, 250)
+    this.body.setSize(250, 250)
     this.scene = scene;
+    this.addAnimation();
+    this.play('zombie_walk');
+    // this.scene.tweens.add({
+    //   targets: this,
+    //   x: 0,
+    //   duration: 8800,
+    //   ease: 'Linear'
+    // });
   }
 
   update() {
     this.body.velocity.y = 0;
-
-    if (this.scene.cursors.left.isDown)
-    {
-        this.body.velocity.x = -360;
-        this.anims.play('left', true);
-    }
-    else if (this.scene.cursors.right.isDown)
-    {
-        this.body.velocity.x = 360;
-        this.anims.play('right', true);
-    }
-    else {
-      this.body.velocity.x = 0;
-
-      this.anims.play('turn');
-    }
-
-    if (this.scene.cursors.up.isDown && this.body.touching.down){
-      setTimeout(() => {
-        this.body.velocity.y = -1000;  
-      }, 300);        
-    }
+    this.state = "CHASE";
   }
 }
--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -1,16 +1,14 @@
 /* global Phaser */
 import 'phaser';
 import Player from '../Characters/Player';
+import Zombie from '../Characters/Zombie';
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
     super('Game');
   }
 
-  preload() {
-    // load images
-    this.load.image('background', 'assets/main-logo.jpg');
-    this.load.image('ground', 'assets/ui/blue_button02.png');
+  loadHero() {
     /*right movement images hero*/
     this.load.image('hero1', 'assets/characters/hero/jared0055.png');
     this.load.image('hero2', 'assets/characters/hero/jared0056.png');
@@ -33,12 +31,52 @@ export default class GameScene extends Phaser.Scene {
     this.load.image('hero18', 'assets/characters/hero/jared0094.png');
     this.load.image('hero19', 'assets/characters/hero/jared0095.png');
     this.load.image('hero20', 'assets/characters/hero/jared0096.png');
+
   }
 
-  create() {
+  loadZombie() {
+    /*right movement images hero*/
+    this.load.image('zombie1', 'assets/characters/zombie/jared0174.png');
+    this.load.image('zombie2', 'assets/characters/zombie/jared0175.png');
+    this.load.image('zombie3', 'assets/characters/zombie/jared0176.png');
+    this.load.image('zombie4', 'assets/characters/zombie/jared0177.png');    
+    this.load.image('zombie5', 'assets/characters/zombie/jared0178.png');
+    this.load.image('zombie6', 'assets/characters/zombie/jared0179.png');
+    this.load.image('zombie7', 'assets/characters/zombie/jared0180.png');
+    this.load.image('zombie8', 'assets/characters/zombie/jared0181.png');
+    this.load.image('zombie9', 'assets/characters/zombie/jared0182.png');
+    this.load.image('zombie10', 'assets/characters/zombie/jared0183.png');
+    this.load.image('zombie11', 'assets/characters/zombie/jared0184.png');
+    this.load.image('zombie12', 'assets/characters/zombie/jared0185.png');
+    this.load.image('zombie13', 'assets/characters/zombie/jared0186.png');
+    this.load.image('zombie14', 'assets/characters/zombie/jared0187.png');    
+    this.load.image('zombie15', 'assets/characters/zombie/jared0188.png');
+    this.load.image('zombie16', 'assets/characters/zombie/jared0189.png');
+    this.load.image('zombie17', 'assets/characters/zombie/jared0190.png');
+    this.load.image('zombie18', 'assets/characters/zombie/jared0191.png');
+    this.load.image('zombie19', 'assets/characters/zombie/jared0192.png');
+    this.load.image('zombie20', 'assets/characters/zombie/jared0193.png');
+    this.load.image('zombie21', 'assets/characters/zombie/jared0194.png');
+    this.load.image('zombie22', 'assets/characters/zombie/jared0195.png');
+    this.load.image('zombie23', 'assets/characters/zombie/jared0196.png');
+    this.load.image('zombie24', 'assets/characters/zombie/jared0197.png');
+    this.load.image('zombie25', 'assets/characters/zombie/jared0198.png');
+
+  }
+
+  preload() {
+    // load images
+    this.load.image('background', 'assets/main-logo.jpg');
+    this.load.image('ground', 'assets/ui/blue_button02.png');
+    this.loadHero();
+    this.loadZombie();
+    this.width = this.cameras.main.width;
+    this.height = this.cameras.main.height;
+  }
+
+  create() {    
     const bg = this.add.image(400, 0, 'background');
-    
-    this.player = new Player(this, 100, 100, 'hero');
+    this.player = new Player(this, 0, 100, 'hero');
     this.cursors = this.input.keyboard.createCursorKeys();
 
     this.platforms = this.physics.add.staticGroup();
@@ -48,11 +86,27 @@ export default class GameScene extends Phaser.Scene {
     this.platforms.create(50, 250, 'ground');
     this.platforms.create(750, 220, 'ground');
 
-    this.physics.add.collider(this.player, this.platforms);
+    this.physics.add.collider(this.player, this.platforms);    
+
+    this.zombieGroup = this.physics.add.group();
+
+    this.time.addEvent({
+      delay: 2000,
+      callback: function() {
+        let zombie = new Zombie(this, 750, 100, 'zombie');
+        this.physics.add.collider(zombie, this.platforms);
+        this.zombieGroup.add(zombie);  
+      },
+      callbackScope: this,
+      loop: true
+    });
   }
 
   update ()
   {
-    this.player.update()
+    this.player.update();
+    for(let zombie of this.zombieGroup.getChildren()) {
+      zombie.update();
+    }
   }
 }
