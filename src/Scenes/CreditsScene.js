5299823ff8961ea8565383d6f539cd383e1fbf0a
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2021-01-25 20:35:46 -0300

    Add credits for images's authors

--- src/Scenes/CreditsScene.js
+++ src/Scenes/CreditsScene.js
@@ -8,8 +8,69 @@ export default class CreditsScene extends Phaser.Scene {
   }
 
   preload() {
+    this.width = this.cameras.main.width;
+    this.height = this.cameras.main.height;
   }
 
   create() {
+    const credits = this.make.text({
+      x: 50,
+      y: 50,
+      text: 'Credits',
+      style: {
+        font: '20px monospace',
+        fill: '#ffffff',
+      },
+    });
+
+    const unsplash = this.make.text({
+      x: 50,
+      y: 100,
+      text: 'Unsplash: ',
+      style: {
+        font: '20px monospace',
+        fill: '#ffffff',
+      },
+    });
+
+    const logo = this.make.text({
+      x: 100,
+      y: 120,
+      text: 'Logo: Kristaps Ungurs',
+      style: {
+        font: '20px monospace',
+        fill: '#ffffff',
+      },
+    });
+    
+    const fire = this.make.text({
+      x: 100,
+      y: 150,
+      text: 'Fire: reivaxcorp',
+      style: {
+        font: '20px monospace',
+        fill: '#ffffff',
+      },
+    });
+
+    const openGameArt = this.make.text({
+      x: 50,
+      y: 180,
+      text: 'OpenGameArt:',
+      style: {
+        font: '20px monospace',
+        fill: '#ffffff',
+      },
+    }); 
+
+    const heroAndZombies = this.make.text({
+      x: 100,
+      y: 210,
+      text: 'Hero and zombies sprites: Jorge Avila',
+      style: {
+        font: '20px monospace',
+        fill: '#ffffff',
+      },
+    }); 
   }
 }
